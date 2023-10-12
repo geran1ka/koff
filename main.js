3b2653c2cf341f5e1fee10c2349dbe63c93050fa
Geran1ka <roman.khoruzhy@gmail.com>
2023-10-12 13:45:12 +0300

    Добавить пагинацию

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ import { ProductList } from './modules/ProductList/ProductList';
 import { ApiService } from './services/ApiService';
 import { Catalog } from './modules/Catalog/Catalog';
 import { FavoriteService } from './services/StorageService';
+import { Pagination } from 'swiper/modules';
 
 const productSlider = () => {
   Promise.all([
@@ -66,9 +67,13 @@ const init = () => {
       match.route.handler(match);
     }
   })
-  .on('/category', async ({params: {slug}}) => {
-    const {data: products} = await api.getProducts({category: slug});
+  .on('/category', async ({params: {slug, page}}) => {
+    const {data: products, pagination} = await api.getProducts({
+      category: slug,
+      page: page || 1,
+    });
     new ProductList().mount(new Main().element, products, slug);
+    new Pagination().mount(new ProductList().containerElement).update(pagination);
     router.updatePageLinks();
 
   }, {
